5cbdfa7069f1dafb0f3bfdf2b184b7dc45261512
maozdeveloper <fhluky@gmail.com>
2021-03-22 17:48:33 +0700

    add validationForm

---
+++ src/app/modules/classroom/classroom-add/classroom-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classroom-add',
+  templateUrl: './classroom-add.component.html',
+  styleUrls: ['./classroom-add.component.css']
+})
+export class ClassroomAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
